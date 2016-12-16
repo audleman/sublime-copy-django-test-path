9bf40ac4b2311078243e1778277218d4461851c1
Kevin Audleman <kevin.audleman@gmail.com>
2016-12-15 22:52:59 -0800

    way cooler stuff

--- generate_test_classpath.py
+++ generate_test_classpath.py
@@ -2,19 +2,24 @@ import sublime, sublime_plugin
 import re
 
 
-class GenerateTestsClasspathCommand(sublime_plugin.TextCommand):
-    """
-    Generate, and copy to the clipboard, the text string necessary to run the
-    test that your cursor is currently in. Example:
+class RunTestsMixin(object):
 
-        api.tests.test_model.TestUserProfile.test_foo
+    def find_project_root():
+        """
+        Start at current directory. Walk up the tree until we find `manage.py`
+        """
+        MAX_CONFIG_SEARCH_DEPTH = 20
+        while current_directory and tries < MAX_CONFIG_SEARCH_DEPTH:
+            potential_path = os.path.join(current_directory, native_str(name))
+            if os.path.exists(potential_path):
+                editor_config_file = potential_path
+                break
 
-    Caveats:
-        - Expects your code to be in a directory named `src`. Better would be
-          to walk the path until I find the .git directory
-        - May not always work, e.g. if you click above the top test class. In
-          that case it will simply show an error in the status line
-    """
+            new_directory = os.path.split(current_directory)[0]
+            if current_directory == new_directory:
+                break
+            current_directory = new_directory
+            tries += 1
 
     def find_containing_class(self, sel):
         """
@@ -25,6 +30,8 @@ class GenerateTestsClasspathCommand(sublime_plugin.TextCommand):
         distances = [(sel.a - cd.a, cd) for cd in class_defs]
         distances = [d for d in distances if d[0] >= 0]
         distances = sorted(distances, key=lambda x: x[0])
+        if not distances:
+            return None, 99999
         closest = distances[0]
         line = self.view.substr(self.view.full_line(closest[1]))
         name = re.search('class (.*?)\(', line).groups(0)[0]
@@ -46,22 +53,87 @@ class GenerateTestsClasspathCommand(sublime_plugin.TextCommand):
         name = re.search('def (.*?)\(', line).groups(0)[0]
         return name, closest[0]
 
-    def run(self, edit):
-        try:
-            # Get filename, concat everything past 'src'
-            f_parts = self.view.file_name().replace('.py', '').split('/')
-            f_path = '.'.join(f_parts[f_parts.index('src') + 1:])
+    def run_tests(self, test_path):
+        # Launch a SublimeRepl bash shell
+        sublime.active_window().run_command(
+            "run_existing_window_command",
+            {
+                "id": "repl_shell",
+                "file": "config/Shell/Main.sublime-menu"
+            }
+        )
+        cmds = [
+            'cd ~/robot/malta/',
+            'venv/bin/python manage.py test %s' % test_path]
+        for cmd in cmds:
+            sublime.active_window().run_command(
+                'repl_view_write', {
+                    "external_id": "shell",
+                    "text": '\n' + cmd})
+
+
+class RunTestsFileCommand(RunTestsMixin, sublime_plugin.TextCommand):
+    """
+    Runs tests based on the position of the cursor. Can run one of the
+    following, based on where the cursor is:
+
+        - an individual test, if in a function named `test_`
+        - a test class, if in a class starting with `Test`
+        - a test file, if file has `test_` in its name
+        - the module otherwise
+    """
+
+    def get_path(self):
+        # Get filename, concat everything past 'src'
+        file_path = self.view.file_name().replace('.py', '').split('/')
+        file_path = file_path[file_path.index('src') + 1:]
+        in_test_file = 'test' in file_path[-1]
+        if not in_test_file:
+            f_path = '.'.join(file_path[:-1])
+        else:
+            f_path = '.'.join(file_path)
             # Grab cursor point. Use full line to get to start of line
             sel = self.view.full_line(self.view.sel()[0])
             fun_name, fun_dist = self.find_containing_function(sel)
             class_name, class_dist = self.find_containing_class(sel)
-            # Get full line, including trailing newline so we can insert on next
-            # line = self.view.full_line(sel)
-            # Insert the text
-            f_path = '%s.%s' % (f_path, class_name)
-            if fun_dist < class_dist and fun_name != 'setUp':
+            # Add class name if it has Test in it
+            if class_name and 'Test' in class_name:
+                f_path = '%s.%s' % (f_path, class_name)
+            # Add function name if it's test_*
+            if fun_dist < class_dist and fun_name.startswith('test'):
                 f_path = '%s.%s' % (f_path, fun_name)
-            sublime.set_clipboard(f_path)
-            self.view.set_status('key', f_path)
-        except:
-            self.view.set_status('key', 'something went wrong')
+        return f_path
+
+    def run(self, edit):
+        path = self.get_path()
+        self.view.set_status('a_gtp', '')
+        sublime.set_clipboard(path)
+        self.view.set_status('a_gtp', path)
+        self.run_tests(path)
+
+
+class RunTestsFolderCommand(RunTestsMixin, sublime_plugin.WindowCommand):
+    """
+    This class gets called when a user right clicks on a file/folder in the
+    sidebar. We are passed the path and don't need to check the contents for
+    class name / file name like above.
+    """
+
+    def get_path(self, paths):
+        path = paths[0]
+        file_path = path.split('/')
+        last = file_path[-1]
+        if '.py' in last:
+            if 'test_' in last:
+                file_path = file_path[:-1] + [last.replace('.py', '')]
+            else:
+                file_path = file_path[:-1]
+        file_path = file_path[file_path.index('src') + 1:]
+        file_path = '.'.join(file_path)
+        return file_path
+
+    def run(self, paths):
+        path = self.get_path(paths)
+        self.run_tests(path)
+        super(RunTestsFolderCommand, self).run()
+
